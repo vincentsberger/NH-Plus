2c428b865ba331f8ca567c91e219be1d145553d2
Vincent Simonsberger <vincent.simonsberger@wamoco.de>
2024-05-14 12:13:55 +0200

    01-Pfleger: added caregiver model, DAO and controller classes; added caregiver to MainWindowView

--- src/main/java/de/hitec/nhplus/model/Caregiver.java
+++ src/main/java/de/hitec/nhplus/model/Caregiver.java
@@ -12,59 +12,24 @@ import java.util.List;
  * Patients live in a NURSING home and are treated by nurses.
  */
 public class Caregiver extends Person {
-    private SimpleLongProperty pid;
+
+    private SimpleLongProperty cid;
     private final SimpleStringProperty dateOfBirth;
-    private final SimpleStringProperty careLevel;
-    private final SimpleStringProperty roomNumber;
-    private final SimpleStringProperty assets;
     private final List<Treatment> allTreatments = new ArrayList<>();
+    private final SimpleStringProperty telephone;
 
-    /**
-     * Constructor to initiate an object of class <code>Patient</code> with the given parameter. Use this constructor
-     * to initiate objects, which are not persisted yet, because it will not have a patient id (pid).
-     *
-     * @param firstName First name of the patient.
-     * @param surname Last name of the patient.
-     * @param dateOfBirth Date of birth of the patient.
-     * @param careLevel Care level of the patient.
-     * @param roomNumber Room number of the patient.
-     * @param assets Assets of the patient.
-     */
-    public Patient(String firstName, String surname, LocalDate dateOfBirth, String careLevel, String roomNumber, String assets) {
+
+    public Caregiver(String firstName, String surname, String dateOfBirth) {
         super(firstName, surname);
-        this.dateOfBirth = new SimpleStringProperty(DateConverter.convertLocalDateToString(dateOfBirth));
-        this.careLevel = new SimpleStringProperty(careLevel);
-        this.roomNumber = new SimpleStringProperty(roomNumber);
-        this.assets = new SimpleStringProperty(assets);
+        this.dateOfBirth = new SimpleStringProperty(dateOfBirth);
     }
 
-    /**
-     * Constructor to initiate an object of class <code>Patient</code> with the given parameter. Use this constructor
-     * to initiate objects, which are already persisted and have a patient id (pid).
-     *
-     * @param pid Patient id.
-     * @param firstName First name of the patient.
-     * @param surname Last name of the patient.
-     * @param dateOfBirth Date of birth of the patient.
-     * @param careLevel Care level of the patient.
-     * @param roomNumber Room number of the patient.
-     * @param assets Assets of the patient.
-     */
-    public Patient(long pid, String firstName, String surname, LocalDate dateOfBirth, String careLevel, String roomNumber, String assets) {
-        super(firstName, surname);
-        this.pid = new SimpleLongProperty(pid);
-        this.dateOfBirth = new SimpleStringProperty(DateConverter.convertLocalDateToString(dateOfBirth));
-        this.careLevel = new SimpleStringProperty(careLevel);
-        this.roomNumber = new SimpleStringProperty(roomNumber);
-        this.assets = new SimpleStringProperty(assets);
-    }
-
-    public long getPid() {
-        return pid.get();
+    public long getCid() {
+        return cid.get();
     }
 
     public SimpleLongProperty pidProperty() {
-        return pid;
+        return cid;
     }
 
     public String getDateOfBirth() {
@@ -75,74 +40,14 @@ public class Caregiver extends Person {
         return dateOfBirth;
     }
 
-    /**
-     * Stores the given string as new <code>birthOfDate</code>.
-     *
-     * @param dateOfBirth as string in the following format: YYYY-MM-DD.
-     */
-    public void setDateOfBirth(String dateOfBirth) {
-        this.dateOfBirth.set(dateOfBirth);
-    }
+    public String getTelephone() { return telephone.get();}
 
-    public String getCareLevel() {
-        return careLevel.get();
-    }
-
-    public SimpleStringProperty careLevelProperty() {
-        return careLevel;
-    }
-
-    public void setCareLevel(String careLevel) {
-        this.careLevel.set(careLevel);
-    }
-
-    public String getRoomNumber() {
-        return roomNumber.get();
-    }
-
-    public SimpleStringProperty roomNumberProperty() {
-        return roomNumber;
-    }
-
-
-    public void setRoomNumber(String roomNumber) {
-        this.roomNumber.set(roomNumber);
-    }
-
-    public String getAssets() {
-        return assets.get();
-    }
-
-    public SimpleStringProperty assetsProperty() {
-        return assets;
-    }
-
-    public void setAssets(String assets) {
-        this.assets.set(assets);
-    }
-
-    /**
-     * Adds a treatment to the list of treatments, if the list does not already contain the treatment.
-     *
-     * @param treatment Treatment to add.
-     * @return False, if the treatment was already part of the list, else true.
-     */
-    public boolean add(Treatment treatment) {
-        if (this.allTreatments.contains(treatment)) {
-            return false;
-        }
-        this.allTreatments.add(treatment);
-        return true;
-    }
 
     public String toString() {
-        return "Patient" + "\nMNID: " + this.pid +
+        return "Caregiver" + "\nCID: " + this.cid +
                 "\nFirstname: " + this.getFirstName() +
                 "\nSurname: " + this.getSurname() +
-                "\nBirthday: " + this.dateOfBirth +
-                "\nCarelevel: " + this.careLevel +
-                "\nRoomnumber: " + this.roomNumber +
-                "\nAssets: " + this.assets +
-                "\n";
+                "\nDate of Birth: " + this.getDateOfBirth();
+
     }
 }
