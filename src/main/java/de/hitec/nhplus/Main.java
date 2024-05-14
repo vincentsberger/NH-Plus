42a94732fd6debaf0b39a17a86b0d5d176cdfe40
Vincent Simonsberger <vincent.simonsberger@wamoco.de>
2024-05-15 00:16:18 +0200

    02-Login: user login screen

--- src/main/java/de/hitec/nhplus/Main.java
+++ src/main/java/de/hitec/nhplus/Main.java
@@ -13,33 +13,64 @@ import java.io.IOException;
 
 public class Main extends Application {
 
-    private Stage primaryStage;
+    private Stage mainStage;
+    private Stage authStage;
+    private Boolean isLoggedIn = false;
 
     @Override
     public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
+        this.authStage = authStage;
+        this.mainStage = mainStage;
         mainWindow();
     }
 
     public void mainWindow() {
         try {
-            FXMLLoader loader = new FXMLLoader(Main.class.getResource("/de/hitec/nhplus/MainWindowView.fxml"));
-            BorderPane pane = loader.load();
+            if (!isLoggedIn) {
+                FXMLLoader authWindowloader = new FXMLLoader(Main.class.getResource("de/hitec/nhplus/UserAuthentificationView"));
+                BorderPane authWindowPane = authWindowloader.load();
 
-            Scene scene = new Scene(pane);
-            this.primaryStage.setTitle("NHPlus");
-            this.primaryStage.setScene(scene);
-            this.primaryStage.setResizable(true);
-            this.primaryStage.show();
+                Scene scene = new Scene(authWindowPane);
+                this.authStage.setTitle("NH-Plus - Anmelden");
+                this.authStage.setScene(scene);
+                this.authStage.setResizable(true);
+                this.authStage.show();
 
-            this.primaryStage.setOnCloseRequest(event -> {
-                ConnectionBuilder.closeConnection();
-                Platform.exit();
-                System.exit(0);
-            });
-        } catch (IOException exception) {
+                this.authStage.setOnCloseRequest(event -> {
+                    ConnectionBuilder.closeConnection();
+                    Platform.exit();
+                    System.exit(0);
+                });
+            } else {
+
+                FXMLLoader loader = new FXMLLoader(Main.class.getResource("/de/hitec/nhplus/MainWindowView.fxml"));
+                BorderPane pane = loader.load();
+
+                Scene scene = new Scene(pane);
+                this.primaryStage.setTitle("NH-Plus - Deine Praxis-Übersicht");
+                this.primaryStage.setScene(scene);
+                this.primaryStage.setResizable(true);
+                this.primaryStage.show();
+
+                this.primaryStage.setOnCloseRequest(event -> {
+                    ConnectionBuilder.closeConnection();
+                    Platform.exit();
+                    System.exit(0);
+                });
+            }
+        } catch (
+
+        IOException exception) {
             exception.printStackTrace();
         }
+    }
+
+    private void showAuthWindow() {
+        
+    }
+
+    private void showMainWindow() {
+        
     }
 
     public static void main(String[] args) {
