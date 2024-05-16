91a9adeda53def4f89bc2e5d387913fa2a6199a1
Vincent Simonsberger <vincent.simonsberger@wamoco.de>
2024-05-17 00:31:05 +0200

    02-Login: added SceneController to handle different Views

--- src/main/java/de/hitec/nhplus/Main.java
+++ src/main/java/de/hitec/nhplus/Main.java
@@ -1,45 +1,18 @@
 package de.hitec.nhplus;
 
-import de.hitec.nhplus.datastorage.ConnectionBuilder;
+import de.hitec.nhplus.controller.SceneController;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class Main extends Application {
 
-    private Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        mainWindow();
-    }
-
-    public void mainWindow() {
-        try {
-            FXMLLoader loader = new FXMLLoader(Main.class.getResource("/de/hitec/nhplus/MainWindowView.fxml"));
-            BorderPane pane = loader.load();
-
-            Scene scene = new Scene(pane);
-            this.primaryStage.setTitle("NH-Plus - Übersicht");
-            this.primaryStage.setScene(scene);
-            this.primaryStage.setResizable(false);
-            this.primaryStage.show();
-
-            this.primaryStage.setOnCloseRequest(event -> {
-                ConnectionBuilder.closeConnection();
-                Platform.exit();
-                System.exit(0);
-            });
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
+        SceneController sceneController = new SceneController(primaryStage);
+        sceneController.init();
     }
 
     public static void main(String[] args) {
