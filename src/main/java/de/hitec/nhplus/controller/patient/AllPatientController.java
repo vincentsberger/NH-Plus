0ee927017a8ab51a270d8f7d09e5f33d147e470d
Vincent Simonsberger <vincent.simonsberger@wamoco.de>
2024-05-18 22:44:43 +0200

    03-SpalteVermoegensstand: removed column 'assets' from patient model, db, controller and view

--- src/main/java/de/hitec/nhplus/controller/patient/AllPatientController.java
+++ src/main/java/de/hitec/nhplus/controller/patient/AllPatientController.java
@@ -47,9 +47,6 @@ public class AllPatientController {
     private TableColumn<Patient, String> columnRoomNumber;
 
     @FXML
-    private TableColumn<Patient, String> columnAssets;
-
-    @FXML
     private Button buttonDelete;
 
     @FXML
@@ -69,9 +66,6 @@ public class AllPatientController {
 
     @FXML
     private TextField textFieldRoomNumber;
-
-    @FXML
-    private TextField textFieldAssets;
 
     private final ObservableList<Patient> patients = FXCollections.observableArrayList();
     private PatientDao dao;
@@ -102,9 +96,6 @@ public class AllPatientController {
 
         this.columnRoomNumber.setCellValueFactory(new PropertyValueFactory<>("roomNumber"));
         this.columnRoomNumber.setCellFactory(TextFieldTableCell.forTableColumn());
-
-        this.columnAssets.setCellValueFactory(new PropertyValueFactory<>("assets"));
-        this.columnAssets.setCellFactory(TextFieldTableCell.forTableColumn());
 
         //Anzeigen der Daten
         this.tableView.setItems(this.patients);
@@ -182,17 +173,6 @@ public class AllPatientController {
     }
 
     /**
-     * When a cell of the column with assets was changed, this method will be called, to persist the change.
-     *
-     * @param event Event including the changed object and the change.
-     */
-    @FXML
-    public void handleOnEditAssets(TableColumn.CellEditEvent<Patient, String> event){
-        event.getRowValue().setAssets(event.getNewValue());
-        this.doUpdate(event);
-    }
-
-    /**
      * Updates a patient by calling the method <code>update()</code> of {@link PatientDao}.
      *
      * @param event Event including the changed object and the change.
@@ -250,9 +230,8 @@ public class AllPatientController {
         LocalDate date = DateConverter.convertStringToLocalDate(birthday);
         String careLevel = this.textFieldCareLevel.getText();
         String roomNumber = this.textFieldRoomNumber.getText();
-        String assets = this.textFieldAssets.getText();
         try {
-            this.dao.create(new Patient(firstName, surname, date, careLevel, roomNumber, assets));
+            this.dao.create(new Patient(firstName, surname, date, careLevel, roomNumber));
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
@@ -269,7 +248,6 @@ public class AllPatientController {
         this.textFieldDateOfBirth.clear();
         this.textFieldCareLevel.clear();
         this.textFieldRoomNumber.clear();
-        this.textFieldAssets.clear();
     }
 
     private boolean areInputDataValid() {
@@ -283,6 +261,6 @@ public class AllPatientController {
 
         return !this.textFieldFirstName.getText().isBlank() && !this.textFieldSurname.getText().isBlank() &&
                 !this.textFieldDateOfBirth.getText().isBlank() && !this.textFieldCareLevel.getText().isBlank() &&
-                !this.textFieldRoomNumber.getText().isBlank() && !this.textFieldAssets.getText().isBlank();
+                !this.textFieldRoomNumber.getText().isBlank();
     }
 }
