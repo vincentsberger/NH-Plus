30acbf444138d8f4906e05f08286bcf9ce961cf0
Vincent Simonsberger <vincent.simonsberger@wamoco.de>
2024-05-19 00:40:10 +0200

    02-Login: added check for valid login credentials with caregiver db table

--- src/main/java/de/hitec/nhplus/utils/AuthentificationHelper.java
+++ src/main/java/de/hitec/nhplus/utils/AuthentificationHelper.java
@@ -1,8 +1,16 @@
 package de.hitec.nhplus.utils;
 
+
+import java.util.List;
+
+import de.hitec.nhplus.datastorage.CaregiverDao;
+import de.hitec.nhplus.datastorage.DaoFactory;
+import de.hitec.nhplus.model.Caregiver;
+
 public class AuthentificationHelper {
     private String username;
     private String password;
+    private List<Caregiver> caregivers;
 
     public AuthentificationHelper() {
         this.username = "admin";
@@ -10,13 +18,29 @@ public class AuthentificationHelper {
     }
 
     public boolean isValidLogin(String username, String password) {
-        if(username.equals(this.username) && password.equals(this.password)) {
+        /** check fot user from caregiver table from DB */
+        this.fetchCaregiverUserCredentialsFromDB();
+        for (Caregiver pfleger : this.caregivers) {
+            if(username.equals(pfleger.getUsername())) {
+                if (password.equals(pfleger.getPassword())) {
+                    return true;
+                }
+            }
+        }
+        /** check for local admin */
+        if (username.equals(this.username) && password.equals(this.password)) {
             return true;
         }
         return false;
     }
 
     public void fetchCaregiverUserCredentialsFromDB() {
+        CaregiverDao cDao = DaoFactory.getDaoFactory().createCaregiverDao();
+        try {
+            this.caregivers = cDao.readAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
